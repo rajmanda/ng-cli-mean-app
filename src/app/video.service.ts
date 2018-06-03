9f66ccb80436a6194c5fdcf55b7a2d51d0aff43e
Raj Manda <rajaram.manda@dharbor.com>
2018-06-03 00:06:09 -0400

    Initial Commit

---
+++ src/app/video.service.ts
@@ -0,0 +1,66 @@
+import { Video } from './video';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { environment } from './../environments/environment';
+
+@Injectable()
+export class VideoService {
+
+  private _getUrl = environment._getUrl;
+  private _postUrl = environment._postUrl;
+  private _putUrl = environment._putUrl;
+  private _deleteUrl = environment._deleteUrl;
+
+  constructor(private _http: Http) { }
+
+  getVideos() {
+    return this._http.get(this._getUrl)
+      .map((response: Response) => response.json());
+  }
+
+  addVideo(video: Video) {
+
+    this.uvideo._id = video._id;
+    this.uvideo.description = video.description;
+    this.uvideo.title = video.title;
+
+    const findstring = 'watch?v=';
+
+    if (video.url.indexOf(findstring) >= 0) {
+      this.uvideo.url = video.url.replace(findstring, 'embed/');
+    } 
+    
+    
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this._http.post(this._postUrl, JSON.stringify(this.uvideo), options)
+      .map((response: Response) => response.json());
+  }
+
+  uvideo: Video = new Video();
+  updateVideo(video: Video) {
+    
+    this.uvideo._id = video._id;
+    this.uvideo.description = video.description;
+    this.uvideo.title = video.title;
+
+    const findstring = 'watch?v=';
+
+    if (video.url.indexOf(findstring) >= 0) {
+      this.uvideo.url = video.url.replace(findstring, 'embed/');
+    } 
+ 
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this._http.put(this._putUrl + this.uvideo._id, JSON.stringify(this.uvideo), options)
+      .map((response: Response) => response.json());
+  }
+
+  deleteVideo(video: Video) {
+    return this._http.delete(this._deleteUrl + video._id)
+      .map((response: Response) => response.json());
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+   _getUrl: "http://localhost:3000/api/videos",
+   _postUrl: "http://localhost:3000/api/video",
+   _putUrl : "http://localhost:3000/api/video/",
+   _deleteUrl: "http://localhost:3000/api/video/",
+};
